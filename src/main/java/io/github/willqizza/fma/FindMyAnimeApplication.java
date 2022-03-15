ce5c3b6810ac1c722339a16a04c39955db7eb7c4
WillQi <williamqipizza@gmail.com>
2022-03-15 00:27:20 -0600

    initial spring commit

---
+++ src/main/java/io/github/willqizza/fma/FindMyAnimeApplication.java
@@ -0,0 +1,13 @@
+package io.github.willqizza.fma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyAnimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/willqizza/fma/FindMyAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.willqizza.fma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
